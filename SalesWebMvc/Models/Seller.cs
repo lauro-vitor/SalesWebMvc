3ec7b5a86307730d30903eeca026a2821e1b5af5
lauro-vitor <laurovpina@gmail.com>
2020-05-24 10:00:13 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<SalesRecord> Sales { get; set; } 
+        public Seller() { }
+
+        public Seller(
+            int id, 
+            string name, 
+            string email, 
+            DateTime birthDate, 
+            double baseSalary, 
+            Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+            Sales = new List<SalesRecord>();
+        }
+       public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+        public double TotalSales(DateTime intial, DateTime final)
+        {   
+
+            return 0;
+        }
+    }
+}
