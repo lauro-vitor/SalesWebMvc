6cbb3dd8aff2053a8abe8b60c841001f9e268b81
lauro-vitor <laurovpina@gmail.com>
2020-05-24 11:43:58 -0300

    MySql adaptation and first migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,21 +9,22 @@ namespace SalesWebMvc.Models
     {   
         public int Id { get; set; }
         public string Name { get; set; }
-        List<Seller> Sellers { get; set; }
+        //List<Seller> Sellers { get; set; }
+        public Department() { }
         public Department(int id, string name)
         {
             Id = id;
             Name = name;
-            Sellers = new List<Seller>();
+            //Sellers = new List<Seller>();
         }
-        public void AddSeller(Seller seller)
+       /* public void AddSeller(Seller seller)
         {
             Sellers.Add(seller);
         }
         public double TotalSeles(DateTime intial, DateTime final)
         {
             return 0;
-        }
+        }*/
        
     }
 }
